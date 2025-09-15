88e9b685fa94d9e2665b22749d5945e3dd7bf80f
PepperShaker <martherssteve@gmail.com>
2025-09-15 23:47:11 +0800

    update clock demo

--- Assets/Script/Clock.cs
+++ Assets/Script/Clock.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Clock : MonoBehaviour
 {
+    const float hours2degrees = -30f;
+    const float minutes2degrees = -6f;
+    const float seconds2degrees = -6f;
+    
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+    
+    // [SerializeField]
+    // Transform minutesPivot;
+    
+    // [SerializeField]
+    // Transform secondsPivot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,19 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // var time = DateTime.Now;
+        // Debug.Log(DateTime.Now.Hour);
         
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        
+        /*
+         hoursPivot.rotation = Quaternion.Euler(0f, 0f, hours2degrees * time.Hour);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutes2degrees * time.Minute);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, seconds2degrees * time.Second);
+        */
+        
+        hoursPivot.rotation = Quaternion.Euler(0f, 0f, hours2degrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutes2degrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, seconds2degrees * (float)time.TotalSeconds);
     }
 }
