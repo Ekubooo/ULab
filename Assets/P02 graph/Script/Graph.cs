258c9a8ade9f6baeaccf3e9821a2680ff18c2fb1
PepperShaker <martherssteve@gmail.com>
2025-09-17 22:31:42 +0800

    dynamic sin func

--- Assets/P02 graph/Script/Graph.cs
+++ Assets/P02 graph/Script/Graph.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
@@ -11,16 +8,21 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    Transform[] points;
+
     void Awake()
     {
         float step = 2f / resolution;
         var scale = Vector3.one * step;
         var position = Vector3.zero;
-        for (int i = 0; i < resolution; i++)
+        
+        points = new Transform[resolution];
+        
+        for (int i = 0; i < points.Length; i++)
         {
-            Transform point = Instantiate(pointPrefab);
+            Transform point = points[i] = Instantiate(pointPrefab);
+            
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -36,6 +38,14 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            // position.y = position.x * position.x * position.x;
+            position.y = Mathf.Sin(Mathf.PI *  (position.x + time));
+            point.localPosition = position;
+        }
     }
 }
