37a35ec5b880283f7b7489a997360e07db4d1505
PepperShaker <martherssteve@gmail.com>
2025-09-19 19:53:01 +0800

    XD

---
+++ Assets/P02 graph/Script/FuncLibXD.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using static UnityEngine.Mathf;
+public static class FuncLibXD
+{
+    public delegate Vector3 Func(float x, float z, float t);
+    public enum FuncName{Wave, MultiWave, CrossWave, Ripple, Sphere, Tours, Tours2, ToursR}
+
+    static Func[] _funcs = {Wave, MultiWave, CrossWave, Ripple, Sphere, Tours,  Tours2,  ToursR};
+
+    public static Func GetFunc(FuncName name)
+    {
+        return _funcs[(int)name];
+    }
+    
+    // not for object template, though useing static
+    public static Vector3 Wave(float u, float v, float t)
+    {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
+    }
+    
+    public static Vector3 MultiWave(float u, float v, float t)
+    {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y *= 2f / 3f;
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 CrossWave(float u, float v, float t)
+    {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= 1f / 2.5f;
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 Ripple(float u, float v, float t)
+    {
+        float d = Sqrt(u * u + v * v);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= 1f + 10f * d;
+        p.z = v;
+        return p;
+    }
+
+    public static Vector3 Sphere(float u, float v, float t)
+    {   // uv ball
+        // float r = 0.5f + 0.5f * Sin(PI * t);         // dynamic sphere
+        // float r = 0.9f + 0.1f * Sin(8f * PI * u);    // virtical strap
+        // float r = 0.9f + 0.1f * Sin(8f * PI * v);    // horizon strap
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+        float s = r * Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(PI * 0.5f * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    public static Vector3 Tours(float u, float v, float t)
+    {
+        float r = 1f;
+        float s = 0.5f + r * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+    public static Vector3 Tours2(float u, float v, float t)
+    {
+        float r1 = 0.75f;
+        float r2 = 0.25f;
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+    
+    public static Vector3 ToursR(float u, float v, float t)
+    {
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+}
+
---
+++ Assets/P02 graph/Script/GraphXD.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+public class GraphXD : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+    
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+    
+    [SerializeField]
+    FuncLibXD.FuncName func;
+
+    Transform[] points;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        
+        points = new Transform[resolution * resolution];
+        
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // FuncLib.Func f = FuncLib.GetFunc2(func);
+        FuncLibXD.Func f = FuncLibXD.GetFunc(func);
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) *  step - 1f;
+            }
+            float u = (x + 0.5f) *  step - 1f;
+            points[i].localPosition = f(u, v, time);
+        }
+    }
+}
