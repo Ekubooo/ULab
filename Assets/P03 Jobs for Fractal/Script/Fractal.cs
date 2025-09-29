ccbfa561a9f5d5a5b48d58562b427af97835190f
PepperShaker <martherssteve@gmail.com>
2025-09-29 23:36:22 +0800

    test toolbox remote

--- Assets/P03 Jobs for Fractal/Script/Fractal.cs
+++ Assets/P03 Jobs for Fractal/Script/Fractal.cs
@@ -4,12 +4,13 @@ public class Fractal : MonoBehaviour
 {
     [SerializeField, Range(1, 8)] int depth = 4;
 
-    Fractal FractalChild(Vector3 direction)
+    Fractal FractalChild(Vector3 direction, Quaternion rotation)
     {
         Fractal child = Instantiate(this);
         child.depth = depth - 1;
         child.transform.SetParent(transform, false);
         child.transform.localPosition = direction * 0.75f;
+        child.transform.localRotation = rotation;
         child.transform.localScale = Vector3.one * 0.5f;
         return child;
     }
@@ -18,15 +19,23 @@ public class Fractal : MonoBehaviour
     {
         name = "Fractal" + depth;
         if (depth <= 1) return;
-        Fractal A = FractalChild(Vector3.right);
+        
+        Fractal A = FractalChild(Vector3.up, Quaternion.identity);
+        Fractal B = FractalChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+        Fractal C = FractalChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+        Fractal D = FractalChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
+        Fractal E = FractalChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
+        
         A.transform.SetParent(transform, false);
-        Fractal B = FractalChild(Vector3.up);
         B.transform.SetParent(transform, false);
+        C.transform.SetParent(transform, false);
+        D.transform.SetParent(transform, false);
+        E.transform.SetParent(transform, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, 22.5f * Time.deltaTime, 0f);
     }
 }
--- readme.md
+++ readme.md
@@ -1,11 +1,4 @@
 ## todo
-- time flip and lerp
-
-1. two state lerp by time [(ceil & floor)* time backward]
-2. rise by time 
-3. trans order
-
-- trans func: offical 
-``` 
+- nothing
 
 
